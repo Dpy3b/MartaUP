bbfb79c14d692bc235009e81c082cfea6e018066
Dpy3b <57526052+Dpy3b@users.noreply.github.com>
2021-06-23 19:43:49 +0300

    анимации при скролле c либой AOS js

--- js/main-es6.js
+++ js/main-es6.js
@@ -1,19 +1,80 @@
 $(function(){
 
+
     $(".header-slider").slick({
         arrows: false,
         vertical: true,
         verticalSwiping: true,
         dots: true,
         dotsClass: "header-dots",
-        autoplay: 2000      
+        autoplay: 2000
     });
 
-    
-$('.header__burger').on('click', function(){
-    $('.header__list').slideToggle();
+
+    $('.header__burger').on('click', function(){
+        $('.header__list').slideToggle();
+    });
+
+    AOS.init({
+        // Global settings:
+        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+        offset: 100, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 400, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: true, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+      });
+
 });
 
+// const animItems = document.querySelectorAll('._anim-items');
 
-});
+// if (animItems.length > 0){
+//     function animOnScroll(params){
+//         for (let index = 0; index < animItems.length; index++) {
+//             const animItem = animItems[index];
+//             const animItemHeight = animItem.offsetHeight;
+//             animItemOffset = offset(animItem).top;
+//         }
+//     }
+//     function offset(el){
+//         const rect = el.getBoundingClientRect();
 
+//     }
+// }
+
+// You can also pass an optional settings object
+// below listed default settings
+// AOS.init({
+//     // Global settings:
+//     disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+//     startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+//     initClassName: 'aos-init', // class applied after initialization
+//     animatedClassName: 'aos-animate', // class applied on animation
+//     useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+//     disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+//     debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+//     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+//     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+//     offset: 100, // offset (in px) from the original trigger point
+//     delay: 0, // values from 0 to 3000, with step 50ms
+//     duration: 400, // values from 0 to 3000, with step 50ms
+//     easing: 'ease', // default easing for AOS animations
+//     once: false, // whether animation should happen only once - while scrolling down
+//     mirror: false, // whether elements should animate out while scrolling past them
+//     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+//   });
